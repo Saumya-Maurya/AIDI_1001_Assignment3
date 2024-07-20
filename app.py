2f370afb99f7ff11fd00ea58ac7bd8c38346c895
Saumya-Maurya <200553573@student.gerogianc.on.ca>
2024-07-20 18:56:40 -0400

    fix app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, jsonify
 
-app = Flask(_name_)
+app = Flask(__name__)
 
 # Route to return student number
 @app.route('/')
@@ -19,11 +19,11 @@ def webhook():
     if intent == 'Gadget Info':
         response_text = "Here is the information about the Gadgets."
     elif intent == 'Store Hours':
-        response_text = "Here you can see the the timings of the store."
+        response_text = "Here you can see the timings of the store."
     elif intent == 'Tech Categories':
-        response_text = "You can find the catagories available in the store."
+        response_text = "You can find the categories available in the store."
     elif intent == 'Promotions and Discounts':
-        response_text = "Here are the information about the discounts going on the products."
+        response_text = "Here is the information about the discounts on the products."
     else:
         response_text = "I'm not sure how to respond to that."
 
@@ -32,5 +32,5 @@ def webhook():
         "fulfillmentText": response_text
     })
 
-if _name_ == '_main_':
+if __name__ == '__main__':
     app.run(debug=True)
